8a28658d7109f979c2c42369f9da2b37baf4d33b
abubaker <mohammad_abubaker@yahoo.com>
2021-01-02 16:47:21 +0500

    Adding interactivity

--- app/src/main/java/org/abubaker/aboutme/MainActivity.kt
+++ app/src/main/java/org/abubaker/aboutme/MainActivity.kt
@@ -15,16 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // When the DONE button will be pressed
         findViewById<Button>(R.id.done_button).setOnClickListener{
             addNickName(it) // it = done_button | view passed as the argument
         }
+
+        // When the TextField will be clicked
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickName(it)
+        }
     }
 
+    // Function: Add Nickname
     private fun addNickName(view: View) {
         val editText = findViewById<EditText>(R.id.nickname_edit)
         val nicknameTextVeiw = findViewById<TextView>(R.id.nickname_text)
 
         nicknameTextVeiw.text = editText.text
+
+        // Hide Edit field and Show new Nickname
         editText.visibility = View.GONE
         nicknameTextVeiw.visibility = View.VISIBLE
 
@@ -32,4 +41,18 @@ class MainActivity : AppCompatActivity() {
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
+
+    // Function: Update Nickname
+    private fun updateNickName (view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        // Show: Edit field and Done buttons
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+
+        // Hide the Done button
+        view.visibility = View.GONE
+    }
+
 }
