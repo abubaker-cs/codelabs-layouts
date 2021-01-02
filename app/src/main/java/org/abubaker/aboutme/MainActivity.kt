c9182fa6b193d7cd4eea958ffc27e278894dc7f0
abubaker <mohammad_abubaker@yahoo.com>
2021-01-02 16:37:42 +0500

    Adding interactivity...

--- app/src/main/java/org/abubaker/aboutme/MainActivity.kt
+++ app/src/main/java/org/abubaker/aboutme/MainActivity.kt
@@ -1,11 +1,35 @@
 package org.abubaker.aboutme
 
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener{
+            addNickName(it) // it = done_button | view passed as the argument
+        }
+    }
+
+    private fun addNickName(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextVeiw = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextVeiw.text = editText.text
+        editText.visibility = View.GONE
+        nicknameTextVeiw.visibility = View.VISIBLE
+
+        // Hide the Keyboard when the user will press DONE
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
